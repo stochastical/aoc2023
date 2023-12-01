7393888a054ffbffdb950c875da95e1a841e9b9a
Yossi Frenkel <yossi.frenkel@icloud.com>
2023-12-02 01:10:40 +1100

    253 characters

--- day01/day_01_golf.py
+++ day01/day_01_golf.py
@@ -1,1 +1,1 @@
-(p:='one two three four five six seven eight nine'.split(),sum(map(lambda x:int(x[0]+x[-1]),[''.join([[['',s[0]][s[0].isdigit()],str(p.index(w)+1)][s.startswith(w)]for s in[l[i:]for i in range(len(l))]for w in p])for l in open('input.txt').readlines()],)),)[1]
+(p:='one two three four five six seven eight nine'.split(),sum([int(x[0]+x[-1])for x in[''.join([[['',s[0]][s[0].isdigit()],str(p.index(w)+1)][s.startswith(w)]for s in[l[i:]for i in range(len(l))]for w in p])for l in open('input.txt').readlines()]]))[1]
