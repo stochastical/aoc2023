55d361c1044a64c69f212de3d5057bc6aaae553b
Yossi Frenkel <yossi.frenkel@icloud.com>
2023-12-02 13:20:01 +1100

    249 expression

--- day01/day_01_golf.py
+++ day01/day_01_golf.py
@@ -1,5 +1,4 @@
-# as a single expression (253):
-(p:='one two three four five six seven eight nine'.split(),sum([int(x[0]+x[-1])for x in[''.join([[['',s[0]][s[0].isdigit()],str(p.index(w)+1)][s.startswith(w)]for s in[l[i:]for i in range(len(l))]for w in p])for l in open('input.txt').readlines()]]))[1]
-
+# as a single expression (249):
+(p:='one two three four five six seven eight nine'.split(),sum(int(x[0]+x[-1])for x in[''.join([['',s[0]][s[0].isdigit()],str(p.index(w)+1)][s.startswith(w)]for s in[l[i:]for i in range(len(l))]for w in p)for l in open('input.txt').readlines()]))[1]
 # allowing statements (243):
 p="one two three four five six seven eight nine".split();sum(int(x[0]+x[-1])for x in["".join([["",s[0]][s[0].isdigit()],str(p.index(w)+1)][s.startswith(w)]for s in[l[i:]for i in range(len(l))]for w in p)for l in open("input.txt").readlines()])
